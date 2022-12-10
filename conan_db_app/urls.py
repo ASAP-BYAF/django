de701a586ea38796509a2039d2230ba8fefdf8b6
teppei <teppei0830.4869@gmail.com>
2022-12-10 15:05:37 +0900

    概要 フッターナビゲーションのリンクを作成しました。
    詳細
    人物リスト以外のリンク先はまだ作っていないのでホームに飛ぶようになっています。

--- conan_db_app/urls.py
+++ conan_db_app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('chara_list/', views.CharaListView.as_view(), name='list'),
     path('chara_detail/<int:pk>', views.chara_detail_func, name='detail'),
+    path('case_list/', views.IndexView.as_view(), name='case'),
+    path('wiseword_list/', views.IndexView.as_view(), name='wiseword'),
+    path('others/', views.IndexView.as_view(), name='others'),
 ]
