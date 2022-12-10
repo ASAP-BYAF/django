59c14ee14e632dece31451ecb8baaa3ae82f2486
teppei <teppei0830.4869@gmail.com>
2022-12-10 16:51:21 +0900

    概要 character_detail.html の登場話数が多くなることが予想されたのでページネーションを作成しました。
    詳細
    urls.py では、これまでのパスに /<int:page> を加えて、ページ数を受け取るようにしました。
    
    views.py の chara_deital_func のほうでも引数に page を追加しました。
    また、ページネーションを作るためのモジュール pagenator を import してます。
    
    character_detail.html のほうではページネーションのリンクを作成しました。
    リンク先は設定されていますが、デザインはまだできていません。
    これから実装する予定です。

--- conan_db_app/views.py
+++ conan_db_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from .models import Character, Chapter
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -12,14 +13,16 @@ class IndexView(TemplateView):
 class CharaListView(ListView):
     model = Character
 
-def chara_detail_func(request, pk):        
+def chara_detail_func(request, pk, page):        
 
     character = Character.objects.get(id = pk)
     chapter = character.chapter.all().order_by('volume_id', 'number')
+    chapter_page = Paginator(chapter, 5)
     event = character.event_related_to.all()
     context ={
         'character' : character, # html 側で for で回すので、イテラブルとして渡す。
-        'chapter_list': chapter,
+        # 'chapter_list': chapter,
+        'chapter_list': chapter_page.get_page(page),
         'event_list': event,
     }
     return render(request, 'conan_db_app/character_detail.html', context)
