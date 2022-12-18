1d44b3e225c5de1ff0995f00cb3ce38729240b49
teppei <teppei0830.4869@gmail.com>
2022-12-18 14:49:37 +0900

    form.py において QuestionForm のフィールドを追加しました。
    これは、モデル Question のフィールドが追加されたことに対応するためです。

--- conan_db_app/forms.py
+++ conan_db_app/forms.py
@@ -4,7 +4,7 @@ from .models import Question
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ['kind','content','n_option','option1','option2','option3','option4','option5']
+        fields = ['jenre', 'kind','content','n_option','option1','option2','option3','option4','option5']
 
     def clean_option1(self):
         option1 = self.cleaned_data["option1"]
