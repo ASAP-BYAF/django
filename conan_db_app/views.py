9b78326c3cb8acc60881bdceacf657a79b3ac1fe
teppei <teppei0830.4869@gmail.com>
2023-01-17 21:34:17 +0900

    question_list.html においてジャンルと種類ごとにページネーションが機能するようにしました。
    ジャンルと種類を指定するフォームの値を js で読み取り、遷移先の url にクエリパラメータを追加することで
    実現しています。

--- conan_db_app/views.py
+++ conan_db_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.views.generic.edit import FormMixin
 from .models import Character, Chapter, Question
-from .forms import QuestionForm
+from .forms import QuestionForm, RefineQuestionForm
 from django.core.paginator import Paginator
 from django.urls import reverse_lazy
 from django.contrib import messages  # メッセージフレームワーク
@@ -45,7 +46,39 @@ class QuestionCreateView(CreateView):
         messages.warning(self.request, "保存できませんでした")
         return super().form_invalid(form)
 
-class QuestionListView(ListView):
+class QuestionListView(ListView, FormMixin):
     model = Question
     template_name = 'conan_db_app/question_list.html'
     paginate_by = 3
+    form_class = RefineQuestionForm
+    
+    def get_queryset(self):
+        # POST リクエストパラメータがあれば、それでフィルタする
+        if self.request.POST:
+            queryset = Question.objects.filter(jenre=self.request.POST['jenre'])\
+                .filter(kind=self.request.POST['kind'])
+
+        # GETリクエストパラメータがあれば、それでフィルタする
+        elif self.request.GET:
+            # queryset = queryset.object.filter(jenre=keyword)
+            queryset = Question.objects.filter(jenre=self.request.GET.get('jenre'))\
+                .filter(kind=self.request.GET.get('kind'))
+
+        # 最初のアクセス時はリクエストパラメータが存在しないので全質問データを返す。
+        else:
+            queryset = Question.objects.all()
+        return queryset
+        
+    def get_context_data(self, **kwargs):
+        print(self.request.GET)
+        if not self.request.GET:
+            # Do this...
+            print('null')
+            return super().get_context_data(test='test')
+        else:
+            # GET リクエストパラメータが存在するときこれをフォームにセットする。
+            return super().get_context_data(form=self.form_class(self.request.GET))
+    
+    def post(self, request):
+        form = self.form_class(request.POST)
+        return self.get(request)
--- static/conan_db_app/js/main.js
+++ static/conan_db_app/js/main.js
@@ -1,24 +1,63 @@
+// ページ読み込み時にページネーションで別ページがある場合には
+// フォームの値をクエリパラメータに設定して、
+// ページネーションで用意された別のページに遷移しても
+// フォームで指定した種類のみでページネーションが行われるようにする。
+
+// 前のページがあるときの処理が未設定。
+
+window.addEventListener("load", setQueryParam);
+
+// フォーム要素を取得。
 const valueRefineQuestionForm = document.querySelector('#refine-question-form');
 
-valueRefineQuestionForm.addEventListener('change', handleChange);
-
-function handleChange(event) {
-  // 現在の選択状態を取得
+function setQueryParam(event) {
+  // 現在、選択されている分類の値を取得
   const valueKind = valueRefineQuestionForm.kind.value;
   const valueJenre = valueRefineQuestionForm.jenre.value;
 
-  console.log(valueKind)
-  console.log(valueJenre)
+  console.log(valueKind);
+  console.log(valueJenre);
+
+  // 遷移先の url が書かれている要素を取得
+  const url_prev = document.getElementById('question-list-page-prev');
+  const url_first = document.getElementById('question-list-page-first');
+  const url_next = document.getElementById('question-list-page-next');
+  const url_last = document.getElementById('question-list-page-last');
+
+  // 前のページがあるときにのみ prev と first の url に対してクエリパラメータの設定を行う。
+  if (url_prev) {
+    // 遷移先の url のクエリパラメータに form に設定されている値を追加。
+    console.log(url_prev);
+    console.log(url_prev.getAttribute('href'));
+    console.log(url_first);
+    console.log(url_first.getAttribute('href'));
+    const url_prev_with_query = url_prev.getAttribute('href') 
+      + '&jenre=' + valueJenre
+      + '&kind=' + valueKind;
+    const url_first_with_query = url_first.getAttribute('href') 
+      + '&jenre=' + valueJenre
+      + '&kind=' + valueKind;
+    url_prev.setAttribute('href', url_prev_with_query);
+    url_first.setAttribute('href', url_first_with_query);
+  }
+
+  // 次のページがあるときにのみ next と last の url に対してクエリパラメータの設定を行う。
+  if (url_next) {
+    // 遷移先の url のクエリパラメータに form に設定されている値を追加。
+    console.log(url_next);
+    console.log(url_next.getAttribute('href'));
+    console.log(url_last);
+    console.log(url_last.getAttribute('href'));
+    const url_next_with_query = url_next.getAttribute('href') 
+      + '&jenre=' + valueJenre
+      + '&kind=' + valueKind;
+    const url_last_with_query = url_last.getAttribute('href') 
+      + '&jenre=' + valueJenre
+      + '&kind=' + valueKind;
+    url_next.setAttribute('href', url_next_with_query);
+    url_last.setAttribute('href', url_last_with_query);
+  }
 }
 
-const url = new URL(location);
-url.toString();
 
-// if( !url.searchParams.get('mode') ) {
-
-// 	url.searchParams.append('mode','view');
-// 	location.href = url;
-
-// } else {
-// 	console.log(url.searchParams.get('mode')); // view
-// }
+console.log();
